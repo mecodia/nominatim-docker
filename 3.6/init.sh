c8d129674f2627e1d943e3a33681db94cfbfaa90
Dennis Schmalacker <schmalacker@mecodia.de>
2021-02-15 13:24:36 +0100

    Update local

--- 3.6/init.sh
+++ 3.6/init.sh
@@ -1,7 +1,7 @@
 THREADS=4
 
-curl http://download.geofabrik.de/europe/germany-latest.osm.pbf > /data/germany-latest.osm.pbf
-useradd -m -p password1234 nominatim && \
+curl http://download.geofabrik.de/europe/dach-latest.osm.pbf > /data/dach-latest.osm.pbf
+useradd -m -p nominatim nominatim && \
 chown -R nominatim:nominatim ./src && \
-sudo -u nominatim ./src/build/utils/setup.php --osm-file /data/germany-latest.osm.pbf --all --threads $THREADS && \
+sudo -u nominatim ./src/build/utils/setup.php --osm-file /data/dach-latest.osm.pbf --all --threads $THREADS && \
 sudo -u nominatim ./src/build/utils/check_import_finished.php
