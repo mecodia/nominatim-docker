01d4b37bcaca17979e23fec144fe29b8a58724bd
Dennis <schmalacker@mecodia.de>
2021-02-15 11:25:40 +0100

    Update init.sh

--- 3.6/init.sh
+++ 3.6/init.sh
@@ -1,7 +1,7 @@
-OSMFILE=$1
-THREADS=$2
+THREADS=4
 
+curl http://download.geofabrik.de/europe/germany-latest.osm.pbf /data/germany-latest.osm.pbf
 useradd -m -p password1234 nominatim && \
 chown -R nominatim:nominatim ./src && \
-sudo -u nominatim ./src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS && \
+sudo -u nominatim ./src/build/utils/setup.php --osm-file /data/germany-latest.osm.pbf --all --threads $THREADS && \
 sudo -u nominatim ./src/build/utils/check_import_finished.php
